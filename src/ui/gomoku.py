5856295e648d0123f66655ea27781171c2a4552d
hiroki <56486054+kirohi-dev@users.noreply.github.com>
2022-01-19 18:33:49 +0900

    feat: auto generate stub

--- src/ui/gomoku.py
+++ src/ui/gomoku.py
@@ -1,6 +1,7 @@
 import gomoku_engine
 import PySimpleGUI as sg
 
+
 sg.theme("DarkAmber")
 layout = [[sg.Text("some test on row 1")], [sg.Text("tenter"), sg.InputText()], [sg.Button("OK"), sg.Button("Cancel")]]
 
